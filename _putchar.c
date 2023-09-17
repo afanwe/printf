dd4b8292eb7a965acbbd1d7bcda9e876286cabd5
Vuyisanani <vuyisanani@ymail.com>
2023-09-17 13:38:27 -0700

    added _putchar.c function

---
+++ _putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _putchar - will write char c to stdout
+ * @c: char to print
+ *
+ * Return: On success 1, or -1 if error
+ * and errno is set appropriately.
+ */
+
+int _putchar(char c)
+
+{
+	return (write(1, &c, 1));
+}
