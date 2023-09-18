5269fd9b002552b9c44465a5b2f2af9969c8a1ef
Vuyisanani <vuyisanani@ymail.com>
2023-09-18 13:48:20 -0700

    added _strlen.c file

---
+++ _strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - computes length of a string excluding null byte
+ *
+ * @s: pointer to string
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++);
+
+	return(i)
+}
