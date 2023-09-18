b4f94860862e94bb713cfcaa87789f85f5e8945b
Asande Didiza <asande.didiza@gmail.com>
2023-09-18 04:11:55 -0700

    Created a backup for original _printf.c as .b and put in new code in _printf.c

--- _printf.c
+++ _printf.c
@@ -1,52 +1,52 @@
-#include  "main.h"
+#include "main.h"
 
 /**
  * _printf - takes unlimited arguments and returns number of char
- * @format: string to print with formated output
+ * @format: string to print with formatted output
  * @...: all arguments to go with formated string
  * Return: returns number of characters entered
  */
 
 int _printf(const char *format, ...)
 {
-	int printed_count = 0, i;
-
+	int index = 0, i;
 	va_list arg_list;
 
 	if (format == NULL)
 		return (-1);
-
 	va_start(arg_list, format);
-
-	for (i = 0; format[i] != '\0'; i++)
+	while (*format != '\0')
 	{
-		if (format[i] != '%')
-			putchar(format[i]);
-		if (format[i] == '%')
+		if (*format != '%')
 		{
-			if (format[i + 1] == 'c')
-			{
-				putchar(va_arg(arg_list, int));
-				i++;
-			}
-			else if (format[i + 1] == 's')
-			{
-				char *str =  va_arg(arg_list, char*);
-				unsigned int str_count = 0;
-
-				while (str_count < strlen(str))
-				{
-					putchar(str[str_count]);
-					str_count++;
-				}
-				printed_count += str_count;
-				i++;
-			}
-			else if (format[i + 1] == '%')
-				putchar('%');
+			write(1, format, 1);
+			index++;
 		}
-		printed_count += 1;
+		else
+		{
+			format++;
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				index++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(arg_list, char *);
+
+				index += (strlen(str));
+				write(1, str, strlen(str));
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(arg_list, int);
+
+				write(1, &c, 1);
+				index++;
+			}
+		}
+		format++;
 	}
 	va_end(arg_list);
-	return (printed_count);
+	return (index);
 }
