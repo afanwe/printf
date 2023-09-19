e71119dd729a62b31569a6b9c3c1f42b0b6f0200
Asande Didiza <asande.didiza@gmail.com>
2023-09-19 14:20:43 -0700

    Added more code

--- _functions2.c
+++ _functions2.c
@@ -34,3 +34,44 @@ int _binary_print(unsigned int num)
 	return (counter);
 }
 
+/**
+ * _address_print - prints address in hexadecimal format
+ * @p: pointer to print address of
+ * Return: number of characters to print
+ */
+
+int _address_print(void *p)
+{
+	char hex[] = "0123456789abcdef";
+	int counter = 0;
+	uintptr_t addr = (uintptr_t)p;
+
+	_putchar('0');
+	_putchar('x');
+	counter = counter + 2;
+
+	if (addr == 0)
+	{
+		_putchar('0');
+		counter++;
+	}
+	else
+	{
+		int i;
+		char hexa[16];
+
+		for (i = 0; addr > 0; i++)
+		{
+			hexa[i] = hex[addr % 16];
+			addr /= 16;
+		}
+
+		for (i -= 1; i >= 0; i++)
+		{
+			_putchar(hexa[i]);
+			counter++;
+		}
+	}
+	return (counter);
+}
+
--- main.h
+++ main.h
@@ -7,23 +7,10 @@
 #include <limits.h>
 #include <string.h>
 #include <unistd.h>
-
-
-
-/**
- * struct format - match conversion specifiers for printf func
- * @id: type char pointer of specifier
- * @f: type pointer to funct for conversion specifier
- *
- */
-
-typedef struct format
-{
-	char *id;
-	int (*f)();
-} convert_match;
+#include <stdint.h>
 
 int _reverse_str(char *str);
+int _address_print(void *p);
 int rot13(char *str);
 int _strlen(char *s);
 int _binary_print(unsigned int num);
@@ -31,21 +18,5 @@ int _putchar(char c);
 int _puts(char *str);
 int num_printer(int num);
 int _printf(const char *format, ...);
-int printf_pointer(va_list val);
-int printf_hex_aux(unsigned long int num);
-int printf_HEX_aux(unsigned int num);
-int printf_exclusive_string(va_list val);
-int printf_HEX(va_list val);
-int printf_hex(va_list val);
-int printf_oct(va_list val);
-int printf_unsigned(va_list args);
-int printf_bin(va_list val);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int *_strcpy(char *dest, char *src);
-int _strlenc(const char *s);
-int printf_37(void);
-int printf_char(va_list val);
-int printf_string(va_list val);
 
 #endif
